6e0a3085224faf53051d2312cdecf9f2ecedc4d4
Massimo Nicolardi <nicolardi.massimo@gmail.com>
2025-05-12 01:14:59 +0200

    generazione secret

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -5,12 +5,11 @@ sleep 2
 
 # Install backend dependencies
 composer install --no-interaction
-
+php artisan jwt:secret --force
 # Install frontend dependencies
 php artisan db:wait
 # Run artisan commands
 php artisan migrate --force
-php artisan jwt:secret --force
 php artisan db:seed --force
 
 # Start PHP-FPM
